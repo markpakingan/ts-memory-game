06f75778e7ebc6ead2bb35bbec2ae60022caaf4a
markpakingan <116990353+markpakingan@users.noreply.github.com>
2024-01-31 21:46:04 -0800

    added class id on squares

--- dist/square.js
+++ dist/square.js
@@ -1,5 +1,22 @@
 "use strict";
-const createSquare = (length, width) => {
-    console.log("this is working");
+const gameboard = document.querySelector("#gameboard");
+const boardLength = 5;
+const boardWidth = 4;
+const tiles = boardLength * boardWidth;
+const colors = ["red", "orange", "yellow", "green", "blue", "violet",
+    "black", "pink", "purple", "aquamarine"];
+const createGameBoard = () => {
+    for (let i = 0; i < tiles; i++) {
+        const square = document.createElement("div");
+        const squareId = `square-${i}`;
+        square.setAttribute("id", squareId);
+        square.setAttribute("class", "square");
+        square.setAttribute("data-index", i.toString());
+        gameboard === null || gameboard === void 0 ? void 0 : gameboard.append(square);
+        square.addEventListener("click", () => handleClick(squareId));
+    }
 };
-createSquare(5, 6);
+const handleClick = (squareId) => {
+    console.log(`You clicked on ${squareId}`);
+};
+createGameBoard();
--- square.ts
+++ square.ts
@@ -1,8 +1,32 @@
-const createSquare = (length: number, width: number) => {
-    console.log("this is working");
+const gameboard = document.querySelector("#gameboard");
+
+const boardLength = 5; 
+const boardWidth = 4; 
+const tiles = boardLength * boardWidth ;
+
+
+const colors = ["red", "orange", "yellow", "green", "blue", "violet", 
+"black", "pink", "purple", "aquamarine"];
+
+
+const createGameBoard = () => {
+    for (let i = 0; i < tiles; i++){
+        const square = document.createElement("div");
+        const squareId = `square-${i}`
+        square.setAttribute("id", squareId);
+        square.setAttribute("class", "square");
+
+        square.setAttribute("data-index", i.toString());
+        gameboard?.append(square);
+        square.addEventListener("click", () => handleClick(squareId))
+        
+    }
 }
 
+const handleClick = (squareId:string) => {
+    console.log(`You clicked on ${squareId}`);
+}
 
-createSquare(5,6);
+createGameBoard();
 
-// testing
+
