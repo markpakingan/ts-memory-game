2322ac0b8ac769004c7588dfb0ca8726f29c5870
markpakingan <116990353+markpakingan@users.noreply.github.com>
2024-01-31 19:30:12 -0800

    added starter files

---
+++ dist/square.js
@@ -0,0 +1,5 @@
+"use strict";
+const createSquare = (length, width) => {
+    console.log("this is working");
+};
+createSquare(5, 6);
---
+++ square.ts
@@ -0,0 +1,8 @@
+const createSquare = (length: number, width: number) => {
+    console.log("this is working");
+}
+
+
+createSquare(5,6);
+
+// testing
