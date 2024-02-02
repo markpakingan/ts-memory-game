48ee19da816fdb7b6665ff868f21e9733187314d
markpakingan <116990353+markpakingan@users.noreply.github.com>
2024-02-01 21:35:16 -0800

    working on shuffling colors

--- dist/square.js
+++ dist/square.js
@@ -5,10 +5,20 @@ const boardWidth = 4;
 const tiles = boardLength * boardWidth;
 const colors = ["red", "orange", "yellow", "green", "blue", "violet",
     "black", "pink", "purple", "aquamarine"];
+const colorPairs = [...colors, ...colors];
+const shuffledArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+};
+const shuffledColors = shuffledArray(colorPairs, []);
 const createGameBoard = () => {
     for (let i = 0; i < tiles; i++) {
+        let pickedColor = shuffledColors[i];
         const square = document.createElement("div");
-        const squareId = `square-${i}`;
+        const squareId = `square-${pickedColor}`;
         square.setAttribute("id", squareId);
         square.setAttribute("class", "square");
         square.setAttribute("data-index", i.toString());
